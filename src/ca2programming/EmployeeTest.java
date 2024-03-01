0e5b6b0e6acb48a304539577813f40cbd951ee64
Sonia Ndonga <ndongasonia@gmail.com>
2024-03-01 21:35:13 +0000

    Part C for EmployeeTest
    - Make a search and display for loop for employees in projectGroup
    - Created a variable called m

--- src/ca2programming/EmployeeTest.java
+++ src/ca2programming/EmployeeTest.java
@@ -14,7 +14,7 @@ public class EmployeeTest {
     public static void main(String[] args) {
         // Beginning of main method
 
-        //Created 3 Employee Objects which are the following:
+        // Created 3 Employee Objects which are the following:
         Employee joeBloggs = new Employee("Joe Bloggs", "jb@gmail.com");
         Employee annBanana = new Employee("Ann Banana", "ab@gmail.com");
         Employee tomThumb = new Employee("Tom Thumb", "tt@gmail.com");
@@ -22,8 +22,20 @@ public class EmployeeTest {
         // Declared an Array called projectGroup to store the 3 Employee Objects
         Employee[] projectGroup = {joeBloggs, annBanana, tomThumb};
 
-        //Prints out nextEmpNum value to the terminal window
+        // Prints out nextEmpNum value to the terminal window
         System.out.println("Next employee number: " + Employee.getNextEmpNum());
+        
+        // Search and display the names of the employees with the empNum variable called m
+        // Called the variable called m 
+        int m = 0;
+        
+        for (Employee employee : projectGroup){
+            if(employee.getEmpNum() > m){
+                System.out.println("Employee name: " + employee.getName() + "\t"
+                + "Employee number: " + employee.getEmpNum());
+            }
+        }
+        
 
     } // End of main method
 
