717f38c7e59beea91e2296da3ae5e41f0d77edd8
Sonia Ndonga <ndongasonia@gmail.com>
2024-03-01 20:59:42 +0000

    Created a Getters and Setters for Employee
    - Created a setEmail with validation
    - Accessor Methods with values
    - getNextEmpNum reutrns the value of nextEmpNum

--- src/ca2programming/Employee.java
+++ src/ca2programming/Employee.java
@@ -34,4 +34,51 @@ public class Employee {
         this.empNum = nextEmpNum++;
     }
 
+    // Created a Getters and Setters for Employee
+    // Getter method for getName 
+    public String getName() {
+        return name;
+    }
+
+    // Setter method for setName
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter method for getEmail
+    public String getEmail() {
+        return email;
+    }
+
+    // Setter method for setEmail(email) with validation
+    // Value is not accepted if the length is 3 or less.
+    public void setEmail(String email) {
+        if (email.length() > 3) {
+            this.email = email;
+        } else {
+            System.out.println("Invalid email. It must be longer thans 3 characters");
+        }
+
+    }
+
+    // Getter method for getEmpNum
+    public int getEmpNum() {
+        return empNum;
+    }
+
+    // Setter method for setEmpNum
+    public void setEmpNum(int empNum) {
+        this.empNum = empNum;
+    }
+
+    // Getter method for getNextEmpNum to get next employee number
+    public static int getNextEmpNum() {
+        return nextEmpNum;
+    }
+
+    // Settter method for setNextEmpNum with the current value of nextEmpNum
+    public static void setNextEmpNum(int nextEmpNum) {
+        Employee.nextEmpNum = nextEmpNum;
+    }
+
 } // End class for Employee 
