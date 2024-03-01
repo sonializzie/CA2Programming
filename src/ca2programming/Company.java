873d502cb7136cb650ec52f6128063e7ffde41e2
Sonia Ndonga <ndongasonia@gmail.com>
2024-03-01 21:57:45 +0000

    Create Company.java
    - Commented Code

---
+++ src/ca2programming/Company.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca2programming;
+
+/**
+ *
+ * @author sonia_ndonga
+ */
+public class Company {
+    // Beginning class for Company
+    
+}// End class for Company
