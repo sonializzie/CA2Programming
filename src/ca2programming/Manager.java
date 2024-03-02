b6417fe0e01287173195f560e2256946f34c3565
Sonia Ndonga <ndongasonia@gmail.com>
2024-03-02 00:20:44 +0000

    Create Manager.java
    - Commented codes

---
+++ src/ca2programming/Manager.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca2programming;
+
+/**
+ *
+ * @author sonia_ndonga
+ */
+public class Manager {
+    // Beginning class for Manager
+    
+} // End class for Manager
