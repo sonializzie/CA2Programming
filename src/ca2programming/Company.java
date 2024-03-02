68e7c2f48a9af800a3bf41df64f8961703f8369f
Sonia Ndonga <ndongasonia@gmail.com>
2024-03-02 00:13:46 +0000

    Update Company.java
    - Added class attributes for Company
    - Created two constructors for Company
    - Created a addNewStaff method
    -Created a getStaffNumber method
    -Created a listEmployees method
    -Created a removeStaff method

--- src/ca2programming/Company.java
+++ src/ca2programming/Company.java
@@ -4,11 +4,70 @@
  */
 package ca2programming;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  *
  * @author sonia_ndonga
  */
 public class Company {
     // Beginning class for Company
+
+    // Created a class attribute for Company 
+    private String companyName;
+    private ArrayList<Employee> staff;
+
+    // Created a Company default constructor
+    public Company() {
+        this.companyName = "Default Company";
+        this.staff = new ArrayList<>();
+    }
+
+    // Created a Company constructor that is overloaded
+    public Company(String companyName) {
+        this.companyName = companyName;
+        this.staff = new ArrayList<>();
+    }
+
+    // Method to add new employee to the staff ArrayList
+    public void addNewStaff(Employee employee) {
+        staff.add(employee);
+    }
+
+    // Method to get the number of employees in the staff ArrayList
+    public int getStaffNumber() {
+        return staff.size();
+    }
+
+    // Method to list Employees above a given empNum value 
+    public void listEmployee(int empNumValue) {
+        Iterator<Employee> iterator = staff.iterator();
+
+        while (iterator.hasNext()) {
+            Employee employee = iterator.next();
+
+            if (employee.getEmpNum() > empNumValue) {
+                System.out.println("Employee name: " + employee.getName());
+            }
+        }
+    }
     
+    
+    // Method to remove an employee from the staff ArrayList based on the empNum
+    public void removeStaff(int empNumToRemove){
+        Iterator<Employee> iterator = staff.iterator();
+        
+        while (iterator.hasNext()){
+            Employee employee = iterator.next();
+            
+            if (employee.getEmpNum() == empNumToRemove){
+                iterator.remove();
+                System.out.println("Employee has been removed successfully.");
+                return;
+            }
+        }
+        System.out.println("Employee with empNum " + empNumToRemove + " not found. ");
+    }
+
 }// End class for Company
